30a6d8909e4b470517a1c421f9c766f8201198d1
Tom Schutte <tjschutte@wisc.edu>
2017-01-27 13:22:05 -0600

    Initial commit
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

---
+++ counter/counter.ino
@@ -0,0 +1,34 @@
+ 
+#define LEDPIN 13
+
+ 
+#define SENSORPIN 4
+ 
+int sensorState = 0, lastState = 0;
+int count = 0;
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(LEDPIN, OUTPUT);      
+  // initialize the sensor pin as an input:
+  pinMode(SENSORPIN, INPUT);     
+  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  // read the state of the pushbutton value:
+  sensorState = digitalRead(SENSORPIN);
+  /*
+  if (sensorState && !lastState) {
+    Serial.println("Unbroken");
+  } 
+  */
+  if (!sensorState && lastState) {
+    //Serial.println("Broken");
+    count++;
+    Serial.println(count, DEC);
+  }
+  lastState = sensorState;
+}
