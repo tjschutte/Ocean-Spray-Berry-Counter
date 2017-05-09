215d912ecde2a46f6461815bb389a6e39aa092cc
Tom Schutte <tjschutte@users.noreply.github.com>
2017-05-09 12:41:27 -0500

    Update counter.ino
    Updated with interrupts and to detect on rising or falling edges.

--- counter/counter.ino
+++ counter/counter.ino
@@ -1,34 +1,18 @@
- 
-#define LEDPIN 13
+#define SENSORPIN 2
 
- 
-#define SENSORPIN 4
- 
-int sensorState = 0, lastState = 0;
 int count = 0;
 
-void setup() {
-  // initialize the LED pin as an output:
-  pinMode(LEDPIN, OUTPUT);      
-  // initialize the sensor pin as an input:
-  pinMode(SENSORPIN, INPUT);     
-  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
-  
-  Serial.begin(9600);
+void setup() {    
+  pinMode(SENSORPIN, INPUT_PULLUP);     
+  attachInterrupt(digitalPinToInterrupt(SENSORPIN), increment, FALLING | RISING);
+  Serial.begin(115200);
 }
  
 void loop(){
-  // read the state of the pushbutton value:
-  sensorState = digitalRead(SENSORPIN);
-  /*
-  if (sensorState && !lastState) {
-    Serial.println("Unbroken");
-  } 
-  */
-  if (!sensorState && lastState) {
-    //Serial.println("Broken");
-    count++;
-    Serial.println(count, DEC);
-  }
-  lastState = sensorState;
+  Serial.println(count, DEC);
 }
+
+void increment() {
+  count++;
+}
+
